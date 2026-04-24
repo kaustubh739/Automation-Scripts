35876215ba0cfa94224fac747f157c7e8a9bba6f
kaustubh739 <kaustubhwani95@gmail.com>
2026-04-24 20:27:35 +0530

    Add files via upload

---
+++ ProcessKill.py
@@ -0,0 +1,14 @@
+import psutil
+
+def KillProcess(name):
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] == name:
+            print("Killing the process : ",name)
+            proc.kill()
+
+
+def main():
+    KillProcess("msedge.exe")
+
+if __name__ == "__main__":
+    main()
