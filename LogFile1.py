de55c9156a9eee71a04d0e40cd0f8a64be451b01
kaustubh739 <kaustubhwani95@gmail.com>
2026-04-24 20:27:03 +0530

    Add files via upload

---
+++ LogFile1.py
@@ -0,0 +1,22 @@
+import sys
+import time
+def main():
+    timestamp = time.ctime()
+
+    
+    fobj = open("MarvellousLog.log","w")
+
+    Border = "-"*54
+
+    fobj.write(Border+"\n")
+    fobj.write("This is a log file of Kaustubh's Automation Scripts\n")
+    #fobj.write(timestamp)
+    fobj.write(Border+"\n")
+
+    fobj.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    fobj.write(Border+"\n")
+    fobj.write("This is created at\n "+timestamp+"\n")
+    fobj.write(Border+"\n")
+
+if __name__ == "__main__":
+    main()
---
+++ LogFile3.py
@@ -0,0 +1,31 @@
+import sys
+import time
+
+def CreateLog():
+    timestamp = time.ctime()
+
+    filename = "MarvellousLog%s.log" %(timestamp)
+    filename =filename.replace(" ","_")
+    filename =filename.replace(":","_")
+
+
+    fobj = open(filename,"w")
+
+    Border = "-"*54
+
+    fobj.write(Border+"\n")
+    fobj.write("This is a log file of Kaustubh's Automation Scripts\n")
+    #fobj.write(timestamp)
+    fobj.write(Border+"\n")
+
+    fobj.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    fobj.write(Border+"\n")
+    fobj.write("This is created at\n "+timestamp+"\n")
+    fobj.write(Border+"\n")
+
+def main():
+    CreateLog()
+    
+
+if __name__ == "__main__":
+    main()
